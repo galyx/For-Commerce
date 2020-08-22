ab558c4358c3cde336ef38d0984d58c1a696b582
galyx <ellernetpar@gmail.com>
2020-08-21 22:51:51 -0300

    Novos arquivos

---
+++ resources/js/components/Calculadora.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container mx-auto my-5">
+        <form @submit.prevent="calcular">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Largura</label>
+                <input
+                    type="text"
+                    class="form-control"
+                    id="largura"
+                    v-model="largura"
+                />
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Profundidade</label>
+                <input
+                    type="text"
+                    class="form-control"
+                    id="profundidade"
+                    v-model="profundidade"
+                />
+            </div>
+            <button type="submit" class="btn btn-primary">Calcular</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            largura: "",
+            profundidade: ""
+        };
+    },
+    methods: {
+    
+      calcular(){
+           let valor = 5; 
+            alert(this.largura + this.profundidade * valor);
+             
+      }
+    
+  }
+};
+</script>
---
+++ resources/views/site/calculadora.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div id="app">
+<calculadora-component></calculadora-component>
+</div>
+@endsection
